55650088bbc8b4506b984bacdf54292a74eef208
NosiphiwoB <biyelanosiphiwo@gmail.com>
2022-08-25 12:43:48 +0200

    react app

--- src/App.js
+++ src/App.js
@@ -1,44 +1,85 @@
 // import logo from './logo.svg';
-import React, {useState} from 'react'
-import './App.css';
-import Data from './MOCK_DATA.json'
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Data from "./MOCK_DATA.json";
 
 function App() {
- 
-  const [showData, setShowData] =  useState();
-  
+  const [showData, setShowData] = useState();
+  const [search, setSearch] = useState();
+
+  useEffect(() => {
+    handleClick();
+  }, []);
+
   const handleClick = () => {
-    let newDater=Data.filter(data => data['Car_model-year'] >= 2005);
-    console.log('newDater', newDater)
-    setShowData(newDater)
-  } 
-console.log('showData', showData)
+    let newDater = Data.filter((data) => data["Car_model-year"] >= 2005);
+    setShowData(newDater);
+  };
 
+  const handleSearchValue = (e) => {
+    let searchedValue = e.target.value;
+    setSearch(searchedValue);
+  };
 
+  const handleSearch = (_) => {
+    let values = showData.filter(
+      (item) =>
+        item.first_name === search ||
+        item.last_name === search ||
+        item.first_name.toLowerCase() === search
+        );
+        setShowData(values)
+  };
 
   return (
     <div className="App">
-      <input type="input" placeholder='search' />
-      <button onClick={handleClick}>Show List</button>     
+      <div style={{ display: "flex" }}>
+        <input
+          type="input"
+          className="form-control"
+          placeholder="search..."
+          name="search"
+          value={search}
+          onChange={(e) => handleSearchValue(e)}
+        />
+        <button type="button" className="btn btn-info" onClick={handleSearch}>
+          Search
+        </button>
+      </div>
 
-      {showData && showData.map((item)=>{
-    return(
-  <div key={item.id} className="objectname">
-  <div>
-    <li>{item.first_name}</li>
-    <li>{item.last_name}</li>
-    <li>{item.email}</li>
-    <li>{item.gender}</li>
-    <li>{item.Car_model}</li>
-    <li>{item["Car_model-year"]}</li>
-    <li>{item.Company_name}</li>
-
-  </div>
-  </div>
-    );
-  })}
-
-
+      <div /*className="objectname"*/>
+        <div style={{ padding: "3rem" }}>
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">First Name</th>
+                <th scope="col">Last Name</th>
+                <th scope="col">Email</th>
+                <th scope="col">Gender</th>
+                <th scope="col">Car Model</th>
+                <th scope="col">Car Model Year</th>
+                <th scope="col">Company Name</th>
+              </tr>
+            </thead>
+            <tbody>
+              {showData &&
+                showData.map((item, id) => {
+                  return (
+                    <tr key={id}>
+                      <td>{item.first_name}</td>
+                      <td>{item.last_name}</td>
+                      <td>{item.email}</td>
+                      <td>{item.gender}</td>
+                      <td>{item.Car_model}</td>
+                      <td>{item["Car_model-year"]}</td>
+                      <td>{item.Company_name}</td>
+                    </tr>
+                  );
+                })}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
